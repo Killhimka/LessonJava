acb091e807ac5eb24d58c4d5d2f19a262c433904
killhimka <bum_911@mail.ru>
2023-04-14 21:47:19 +0300

    14.04.23

--- src/OOP/Hw/Hw5Discriminant/Discriminant.java
+++ src/OOP/Hw/Hw5Discriminant/Discriminant.java
@@ -15,19 +15,19 @@ public class Discriminant {
     }
 
     public void Disc () {
-        System.out.println("Решаем квадтратное уровнение вида:");
+        System.out.println("Р РµС€Р°РµРј РєРІР°РґС‚СЂР°С‚РЅРѕРµ СѓСЂРѕРІРЅРµРЅРёРµ РІРёРґР°:");
         System.out.println("ax^2 - bx + c = 0");
         disc = b * b - 4 * a * c;
         if (disc < 0){
-            System.out.println("Нет корней.");
+            System.out.println("РќРµС‚ РєРѕСЂРЅРµР№.");
         System.exit(1);
     } else if (disc==0) {
             int x = -b / (2*a);
-            System.out.println("В уравнении 1 корень: x = " + x);
+            System.out.println("Р’ СѓСЂР°РІРЅРµРЅРёРё 1 РєРѕСЂРµРЅСЊ: x = " + x);
         }else {
             double x1 = (-b - Math.sqrt(disc)) / (2*a);
             double x2 = (-b + Math.sqrt(disc)) / (2*a);
-            System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = "+x2);
+            System.out.println("РљРѕСЂРЅРё СѓСЂР°РІРЅРµРЅРёСЏ: x1 = " + x1 + ", x2 = "+x2);
         }
 
 
