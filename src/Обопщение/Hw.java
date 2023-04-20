237b13bb3342e664173a1b55afaf4712e5560d16
killhimka <bum_911@mail.ru>
2023-04-20 13:13:52 +0300

    20.04.23

--- src/Обопщение/Hw.java
+++ src/Обопщение/Hw.java
@@ -1,29 +1,40 @@
 package Обобщения;
 
 public class Hw {
-    public static <T, V> boolean isIn(T x, V [] array){
-        for (V element: array){
-            if (x.equals(element)){
-                return true;
+    public class Hw {
+        public static <T, V> boolean isIn(T x, V [] array){
+            for (V element: array){
+                if (x.equals(element)){
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public static <T, V> boolean isIn1(T x, V [] array){
+            for (V element: array){
+                if (x.equals(element)){
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        public static void main(String[] args) {
+            Integer [] intArray = {1,2,3,4,5};
+            if (isIn(5,intArray)){
+                System.out.println("2 уже есть");
+            }
+            if (isIn1(7,intArray)){
+                System.out.println("7 не входит");
+            }
+            System.out.println();
+            String [] strArray = {"one","two","three","four","five"};
+            if (isIn("two",strArray)){
+                System.out.println("Входит");
+            }
+            if (isIn1("seven",strArray)){
+                System.out.println("Не входит");
             }
         }
-        return false;
-    }
-    public static void main(String[] args) {
-        Integer [] intArray = {1,2,3,4,5};
-        if (isIn(5,intArray)){
-            System.out.println("2 уже есть");
-        }
-        if (isIn(7,intArray)){
-            System.out.println("7 не входит");
-        }
-        System.out.println();
-        String [] strArray = {"one","two","three","four","five"};
-        if (isIn("two",strArray)){
-            System.out.println("Входит");
-        }
-        if (isIn("seven",strArray)){
-            System.out.println("Не входит");
-        }
-    }
 }
