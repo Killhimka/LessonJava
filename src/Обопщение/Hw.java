a5f8a48c7c663208691ed30094bb99265e857b3b
killhimka <bum_911@mail.ru>
2023-04-20 14:11:21 +0300

    20.04.23

--- src/Обопщение/Hw.java
+++ src/Обопщение/Hw.java
@@ -11,20 +11,19 @@ public class Hw {
     }
 
     public static void main(String[] args) {
-
         Integer [] intArray = {1,2,3,4,5};
-        if (isIn(n,intArray)){
+        if (isIn(2,intArray)){
             System.out.println(" уже есть");
         }
-        if (isIn1(n,intArray)){
+        if (!isIn1(7,intArray)){
             System.out.println(" не входит");
         }
         System.out.println();
         String [] strArray = {"one","two","three","four","five"};
-        if (isIn(s,strArray)){
+        if (isIn("two",strArray)){
             System.out.println(" Входит");
         }
-        if (isIn1(s,strArray)){
+        if (!isIn1("seven",strArray)){
             System.out.println(" Не входит");
         }
 
