bd4b25ef9e463c274661e12cf733c03bf99b1773
killhimka <bum_911@mail.ru>
2023-04-20 14:01:14 +0300

    20.04.23

--- src/Обопщение/Hw.java
+++ src/Обопщение/Hw.java
@@ -6,36 +6,26 @@ public class Hw {
             if (x.equals(element)){
                 return true;
             }
-
         }
         return false;
     }
-    public static <T, V> boolean isIn1(T x, V [] array){
-        for (V element: array){
-            if (x.equals(element)){
-                return false;
-            }
 
-        }
-        return true;
-    }
     public static void main(String[] args) {
-        int n = 2;
-        String s = "two";
+
         Integer [] intArray = {1,2,3,4,5};
         if (isIn(n,intArray)){
-            System.out.println(n+" уже есть");
+            System.out.println(" уже есть");
         }
         if (isIn1(n,intArray)){
-            System.out.println(n+" не входит");
+            System.out.println(" не входит");
         }
         System.out.println();
         String [] strArray = {"one","two","three","four","five"};
         if (isIn(s,strArray)){
-            System.out.println(s+" Входит");
+            System.out.println(" Входит");
         }
         if (isIn1(s,strArray)){
-            System.out.println(s+" Не входит");
+            System.out.println(" Не входит");
         }
 
     }
