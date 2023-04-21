ae8a4e1e7c5d32a1994b57aa2a4621fcbbdd4f8e
killhimka <bum_911@mail.ru>
2023-04-21 17:28:46 +0300

    21.04.23

--- src/Обопщение/Hw/Animal.java
+++ src/Обопщение/Hw/Animal.java
@@ -1,38 +1,28 @@
 package Обобщения;
 
-import java.util.Arrays;
-
 class Dog extends Animal{
-    public Dog(String nameAnimal, String colorAnimal) {
-        super(nameAnimal = "Dog", colorAnimal = "Blue");
+    public Dog (){
+        this.nameAnimal = "Dog";
+        this.colorAnimal = "Blue";
     }
 }
 class Cat extends Animal {
-    public Cat(String nameAnimal, String colorAnimal) {
-        super(nameAnimal = "Cat", colorAnimal = "Red");
+    public Cat (){
+        this.nameAnimal = "Cat";
+        this.colorAnimal = "Red";
     }
 }
 public class Animal {
-     String nameAnimal;
-     String colorAnimal;
-
-    public Animal(String nameAnimal, String colorAnimal) {
-        this.nameAnimal = nameAnimal;
-        this.colorAnimal = colorAnimal;
-    }
-
+    String nameAnimal;
+    String colorAnimal;
     public static void main(String[] args) {
-        ClassAnimal classAnimal = new ClassAnimal<>("Name","Color");
-        classAnimal.animal(new Cat("Name","Color"));
+        ClassAnimal classAnimal = new ClassAnimal();
+        classAnimal.animal(new Dog());
     }
 }
-class ClassAnimal <T> extends Animal{
-    public ClassAnimal(String nameAnimal, String colorAnimal) {
-        super(nameAnimal, colorAnimal);
-    }
-
-    public  <T> void animal(Animal ob) {
-        System.out.println(ob.colorAnimal);
-        System.out.println(ob.nameAnimal);
+class ClassAnimal extends Animal{
+    public  <T> void animal(T ob) {
+        System.out.println(((Animal)ob).colorAnimal);
+        System.out.println(((Animal)ob).nameAnimal);
     }
 }
