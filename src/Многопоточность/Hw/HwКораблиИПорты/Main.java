4b47be1de9b169cabd0b69fbed535d07373eced3
killhimka <bum_911@mail.ru>
2023-04-27 19:46:46 +0300

    21.04.23

---
+++ src/Многопоточность/Hw/HwКораблиИПорты/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    static class Product{
+        int cena;
+        String name;
+
+        public Product(int cena, String name) {
+            this.cena = cena;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                    "cena=" + cena +
+                    ", name=" + name +
+                    '}';
+        }
+    }
+    public static void main(String[] args) {
+        HashMap<Integer,String> hm = new HashMap<>();
+        hm.put(10,"Молоко Майонез Хлеб");
+        hm.put(5,"Сок Кефир");
+        hm.put(15,"Сметана Колбаса Грибы");
+        Product product = new Product(10,"Бананы");
+        if (hm.containsKey(product.cena)){
+            hm.put(product.cena, hm.get(product.cena).concat(" "+product.name));
+            String[] sort = hm.get(product.cena).split(" ");
+            Arrays.sort(sort);
+            hm.put(product.cena,Arrays.toString(sort));
+        }   else hm.put(product.cena, Arrays.toString(new String[]{product.name}));
+
+        System.out.println(hm);
+    }
+}
